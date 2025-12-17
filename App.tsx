6749798d10d7c61ce157e562065ac37deedd8b2b
letritrung <letritrung2605@gmail.com>
2025-12-17 14:39:29 +0700

    #9. Todo App - Part 1

--- App.tsx
+++ App.tsx
@@ -1,44 +1,52 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View, TextInput, ScrollView, FlatList } from 'react-native';
-import {useState} from 'react';
+import { useState } from 'react';
+
+interface TodoItem {
+  id: number;
+  title: string;
+}
 
 export default function App() {
-  
-  const [students, setStudents] = useState([
-    {id: 1, name: 'Nguyen Van A', age: 20},
-    {id: 2, name: 'Tran Thi B', age: 21},
-    {id: 3, name: 'Le Van C', age: 22},
-    {id: 4, name: 'Pham Thi D', age: 23},
-    {id: 5, name: 'Hoang Van E', age: 24},
-    {id: 6, name: 'Vu Thi F', age: 25},
-  ]);
-  
-  
-  //jsx 
+
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
+  const [todoInput, setTodoInput] = useState<string>('');
+
+  function handleAddTodo() {
+    if (!todoInput.trim()) {
+      alert('Please enter a valid todo item.');
+      return;
+    }
+    const newTodo: TodoItem = {
+      id: todoList.length + 1,
+      title: todoInput,
+    };
+    setTodoList([...todoList, newTodo]);
+    setTodoInput('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Danh sách sinh viên</Text>
+      <Text style={styles.heading}>Hello React Native</Text>
 
-      {/* Kiểu truyền data thẳng */}
-      <FlatList
-        numColumns={2}
-        data={students}
-        keyExtractor={(item) => item.id.toString()} //nếu không có keyExtractor sẽ bị cảnh báo
-        renderItem={(data) => (
-          <Text style={{flex: 1}}>{data.item.name} - {data.item.age} tuổi</Text>
-        )}
-      />
+      {/* Form */}
+      <View>
+        <Text style={styles.label}>Input item</Text>
+        <TextInput style={styles.input} placeholder='Input item' value={todoInput} onChangeText={setTodoInput}></TextInput>
+        <Button title='Add'
+          onPress={handleAddTodo}
+        ></Button>
+      </View>
 
-      {/* kiểu truyền data object destructuring */}
-      <FlatList
-        data={students}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({item}) => (
-          <View style={{marginTop: 10}}>
-            <Text>{item.name} - {item.age} tuổi</Text>
-          </View>
-        )}
-      />
+      {/* List to-do */}
+      <View>
+        <Text style={styles.label}>List-To-do</Text>
+        <FlatList
+          data={todoList}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => <Text style={styles.todoItem}>{item.title}</Text>}
+        ></FlatList>
+      </View>
     </View>
   );
 }
@@ -47,10 +55,33 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 20,
     paddingVertical: 40,
     flex: 1,
     backgroundColor: '#fff',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginBottom: 20,
+    textAlign: 'center',
+    backgroundColor: 'orange',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#333',
+    padding: 10,
+    marginBottom: 10,
+  },
+  todoItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: '500',
+    marginBottom: 10,
+    left: 10,
   },
 });
 
